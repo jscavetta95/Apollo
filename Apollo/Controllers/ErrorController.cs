c073e08106be2bb6c8338597c49f59e593ebae7e
Joe Scavetta <Jscavetta95@gmail.com>
2018-01-15 21:12:59 -0500

    Error handling page.

---
+++ Apollo/Controllers/ErrorController.cs
@@ -0,0 +1,13 @@
+﻿namespace Apollo.Controllers
+{
+    using System.Web.Mvc;
+
+    public class ErrorController : Controller
+    {
+        // GET: Shared
+        public ActionResult Index()
+        {
+            return View("Error");
+        }
+    }
+}
